36564a2f819d44e193950185437ef1167dfb0863
jmcdermott <jmcdermott@spideroak-inc.com>
2020-10-22 18:59:09 -0500

    Added runbook startup script for review and collab

---
+++ azure-runbook.ps1
@@ -0,0 +1,41 @@
+param(
+    [string] $AutomationAccountName,
+    [string] $resourceGroupName,
+    [string] $storageName,
+    [string] $domainsJson,
+    [string] $emailAddress,
+    [string] $storageContainerName,
+    [string] $AGNamesJson,
+    [string] $AGOldCertName
+)
+# Json variable for domains to be passed into Parameters. Or should this logic be done on the ARM side?
+# Json variable for Gateways to pass in Parameters. Or should this logic be done on the ARM side?
+$letsencryptParameters = @{"domainsJson"=$domainsJson; "emailAddress"=$emailAddress; "resourceGroupName"=$resourceGroupName; "storageName"=$storageName; "storageContainerName"=$storageContainerName; "AGResourceGroupName"=$resourceGroupName; "AGNamesJson"=$AGNamesJson; "AGOldCertName"=$AGOldCertName;};
+
+Start-AzAutomationRunbook -AutomationAccountName $AutomationAccountName -Name letsencryptRunbook -ResourceGroupName $resourceGroupname -MaxWaitSeconds 1000 -Wait -Parameters $letsencryptParameters
+
+<#
+ARM template with scriptcontent, can also use github uri if needed.
+
+Does the runbook script parameters require just the names of resources or the full uri? In the arguments below its just using the name of the resources and no the full resourceid() uri such as:
+resourceId('Microsoft.Storage/blobServices', variables('letsencryptContainerName'))
+
+"properties": {
+    "forceUpdateTag": "1",
+    "azPowerShellVersion": "4.6",
+    "arguments": "[format(' -AutomationAccountName {0} -resourceGroupName {1} -storageName {2} -domainsJson {3} -emailAddress {4} -storageContainerName {5} -AGNamesJson {6} -AGOldCertName {7}', variables('automationAccountName'), resourceGroup().name, parameters('storageAccountName'), 'test.spideroak-domain.com', 'admins@spideroak.com', 'variables('letsEncryptContainerName'), variables('applicationGatewayFlowBlockName'), 'flow')]",
+    "scriptContent": "
+            param(
+                [string] $AutomationAccountName,
+                [string] $resourceGroupName,
+                [string] $storageName,
+                [string] $domainsJson,
+                [string] $emailAddress,
+                [string] $storageContainerName,
+                [string] $AGNamesJson,
+                [string] $AGOldCertName
+            )
+            $letsencryptParameters = @{'domainsJson'=$domainsJson; 'emailAddress'=$emailAddress; 'resourceGroupName'=$resourceGroupName; 'storageName'=$storageName; 'storageContainerName'=$storageContainerName; 'AGNamesJson'=$AGNamesJson; 'AGOldCertName'=$AGOldCertName;}
+            Start-AzAutomationRunbook -AutomationAccountName $AutomationAccountName -Name letsencryptRunbook -ResourceGroupName $resourceGroupname -MaxWaitSeconds 1000 -Wait -Parameters $letsencryptParameters
+            ",
+#>
